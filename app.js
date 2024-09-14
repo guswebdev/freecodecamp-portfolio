584729880eff212db260d9d084ace62ac173c4d9
gusemmanuel <gus.desarrollo.web@gmail.com>
2024-09-14 07:55:45 -0300

    Creacion de un modelo de datos

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import { projects } from "./assets/data.js";
 import { Header } from "./js/componets/Header.js";
 import { click } from "./js/helpers/click.js";
 
 /* --- VARIABLES --- */
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+console.log(projects);
 /* --- FUNCIONES --- */
 const app = () => {
   Header();
---
+++ assets/data.js
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    name: "Tribute Page",
+    tags: ["HTML", "CSS", "JS"],
+    img: "./assets/img/proyecto-tributo.png",
+  },
+  {
+    name: "Survey Form",
+    tags: ["HTML", "CSS", "JS"],
+    img: "./assets/img/proyecto-formulario.png",
+  },
+  {
+    name: "Technical Documentation",
+    tags: ["HTML", "CSS", "JS"],
+    img: "./assets/img/proyecto-documentacion.png",
+  },
+  {
+    name: "Product Landing Page",
+    tags: ["HTML", "CSS", "JS"],
+    img: "./assets/img/proyecto-landing.png",
+  },
+];
